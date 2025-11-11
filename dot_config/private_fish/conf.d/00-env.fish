944940c15002b14b798dc2c436c6e7a84f620435
boze <boze3141@gmail.com>
2025-11-12 00:17:03 +0800

    refactor(env): base env fix

--- dot_config/private_fish/conf.d/00-env.fish
+++ dot_config/private_fish/conf.d/00-env.fish
@@ -1,7 +1,11 @@
+set -gx XDG_CONFIG_DIR "$HOME/.config"
+set -gx XDG_CACHE_HOME "$HOME/.cache"
+set -gx XDG_DATA_HOME "$HOME/.local/share"
+set -gx XDG_STATE_HOME "$HOME/.local/state"
+
 set -gx EDITOR nvim
 set -gx VISUAL nvim
 
 set -g fish_history_limit 10000
 
-set -gx XDG_CONFIG_DIR "/home/boze/.config"
 set -gx VOLTA_HOME "$XDG_CONFIG_DIR/volta"
