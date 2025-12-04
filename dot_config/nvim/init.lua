5ab85436db50fdb532df3aad7ed2563dcfe41fa9
boze <boze3141@gmail.com>
2025-12-04 12:01:39 +0800

    feat(nvim): 添加 chezmoi 模板文件的 filetype 检测

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,2 +1,17 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = "*.tmpl",
+  callback = function()
+    local filename = vim.fn.expand("%:t")
+    -- 提取 .tmpl 之前的扩展名
+    local inner_ext = filename:match("%.([^%.]+)%.tmpl$")
+
+    if inner_ext then
+      -- 根据内层扩展名设置 filetype
+      vim.bo.filetype = inner_ext
+    end
+  end,
+  desc = "Set filetype for chezmoi templates based on inner extension",
+})
