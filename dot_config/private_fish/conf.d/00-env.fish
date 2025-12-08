d7d2e500308a6aed1b7146999fbd5c47034b9ce6
boze <boze3141@gmail.com>
2025-12-08 21:41:16 +0800

    feat(fish): 添加 ~/.local/bin 和 bitwarden ssh agent

--- dot_config/private_fish/conf.d/00-env.fish
+++ dot_config/private_fish/conf.d/00-env.fish
@@ -3,6 +3,7 @@ set -gx XDG_CONFIG_HOME "$HOME/.config"
 set -gx XDG_CACHE_HOME "$HOME/.cache"
 set -gx XDG_DATA_HOME "$HOME/.local/share"
 set -gx XDG_STATE_HOME "$HOME/.local/state"
+fish_add_path ~/.local/bin
 
 # Fish Settings
 set -g fish_history_limit 10000
@@ -14,3 +15,5 @@ set -gx NPM_CONFIG_PREFIX "$XDG_DATA_HOME/npm"
 
 # Node.js REPL 历史记录
 set -gx NODE_REPL_HISTORY "$XDG_DATA_HOME/node_repl_history"
+
+set -gx SSH_AUTH_SOCK "$HOME/.bitwarden-ssh-agent.sock"
